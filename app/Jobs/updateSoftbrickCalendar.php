246d962c8ba673519301120f0a1410d9a2cdd0d3
Jasper Jakobs <github@jasperjakobs.nl>
2021-11-29 22:26:46 +0100

    HTTP 401 error catch

--- app/Jobs/updateSoftbrickCalendar.php
+++ app/Jobs/updateSoftbrickCalendar.php
@@ -44,47 +44,53 @@ class updateSoftbrickCalendar implements ShouldQueue
     {
         if ($this->softbrick->token == null) return;
 
-        $response = Http::acceptJson()->withHeaders([
-            'content-type' => 'application/json'
-        ])->post('https://bcc.softbrick.com:3000/weekrooster?version=4', [
-            'badnum' => $this->softbrick->badnum,
-            'user' => $this->softbrick->email,
-            'token' => $this->softbrick->token,
-            'datumvanaf' =>now()->subWeeks(5)->startOfWeek()->format('Ymd'),
-            'datumtm' => now()->addWeeks(5)->endOfWeek()->format('Ymd'),
-            'language' => 'nl',
-        ])->throw()->json();
+        try {
+            $response = Http::acceptJson()->withHeaders([
+                'content-type' => 'application/json'
+            ])->post('https://bcc.softbrick.com:3000/weekrooster?version=4', [
+                'badnum' => $this->softbrick->badnum,
+                'user' => $this->softbrick->email,
+                'token' => $this->softbrick->token,
+                'datumvanaf' =>now()->subWeeks(5)->startOfWeek()->format('Ymd'),
+                'datumtm' => now()->addWeeks(5)->endOfWeek()->format('Ymd'),
+                'language' => 'nl',
+            ])->throw()->json();
 
-        if ($response["success"] == "false") {
+            if ($response["success"] == "false") {
+                $sbcontroller = new SoftbrickController;
+                $sbcontroller->authSoftbrick($this->softbrick);
+            } else if ($response["success"] == "true") {
+
+                $calendar = Calendar::create('Softbrick van ' . $this->softbrick->email)
+                    ->name('Softbrick van ' . $this->softbrick->email)
+                    ->description('Softbrick werkrooster via https://e2c.jasperjakobs.nl/');
+                $data = $response['data'];
+
+                foreach ($data as $item) {
+                    if ($item['naam'] == 'plan') {
+
+                        $address = "0036: Arnhem";
+
+                        if ($item['locatie'] != null) {
+                            $locations = explode(',', $item['locatie']);
+                            $address = $locations[0];
+                        }
+
+                        $calendar->event(\Spatie\IcalendarGenerator\Components\Event::create()
+                            ->name('Werken')
+                            ->description('Beheerd door e2c.jasperjakobs.nl')
+                            ->address($address)
+                            ->startsAt(new DateTime($item['datum'] . $item['van'], new DateTimeZone('Europe/Amsterdam')))
+                            ->endsAt(new \DateTime($item['datum'] . $item['tot'], new DateTimeZone('Europe/Amsterdam'))));
+                    }
+                }
+
+                Storage::put('public/calendar/feed/' . $this->softbrick->uuid . '.ics', $calendar->get());
+            }
+        } catch (\Exception $exception) {
+            Log::warning("Token incorrect, retrying password.");
             $sbcontroller = new SoftbrickController;
             $sbcontroller->authSoftbrick($this->softbrick);
-        } else if ($response["success"] == "true") {
-
-            $calendar = Calendar::create('Softbrick van ' . $this->softbrick->email)
-                ->name('Softbrick van ' . $this->softbrick->email)
-                ->description('Softbrick werkrooster via https://e2c.jasperjakobs.nl/');
-            $data = $response['data'];
-
-            foreach ($data as $item) {
-                if ($item['naam'] == 'plan') {
-
-                    $address = "0036: BCC Arnhem";
-
-                    if ($item['locatie'] != null) {
-                        $locations = explode(',', $item['locatie']);
-                        $address = $locations[0];
-                    }
-
-                    $calendar->event(\Spatie\IcalendarGenerator\Components\Event::create()
-                        ->name('Werken')
-                        ->description('Beheerd door e2c.jasperjakobs.nl')
-                        ->address($address)
-                        ->startsAt(new DateTime($item['datum'] . $item['van'], new DateTimeZone('Europe/Amsterdam')))
-                        ->endsAt(new \DateTime($item['datum'] . $item['tot'], new DateTimeZone('Europe/Amsterdam'))));
-                }
-            }
-
-            Storage::put('public/calendar/feed/' . $this->softbrick->uuid . '.ics', $calendar->get());
         }
     }
 }
